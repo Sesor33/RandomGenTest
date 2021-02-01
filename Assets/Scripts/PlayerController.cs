0b0aaa4fc2dd3a491631fbbe0bc1dcb07f047692
Tech <sesor333@aol.com>
2021-02-01 12:16:11 -0500

    Working on player movement code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float playerSpeed;
+    Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector3(h * playerSpeed, v * playerSpeed, 0);
+    }
+}
